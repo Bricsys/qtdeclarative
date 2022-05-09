f55e6054f0dc8cccc8e7ee1bd61915c0f0ad5d7e
Ulf Hermann <ulf.hermann@qt.io>
2022-05-09 17:49:17 +0200

    Avoid memory leaks in QQuickListView and tst_qqmlinstantiator
    Change-Id: Ieceffedb082e893b54bcda99076df3ccdeff6010
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
    (cherry picked from commit cf9c0f0180b65f0f2ad2cf20a35a3d11a7430927)

--- src/quick/items/qquicklistview.cpp
+++ src/quick/items/qquicklistview.cpp
@@ -3708,8 +3708,10 @@ bool QQuickListViewPrivate::applyInsertionChange(const QQmlChangeSet::Change &ch
                 item = createItem(it.index, QQmlIncubator::Synchronous);
             if (!item)
                 return false;
-            if (it.removedAtIndex)
+            if (it.removedAtIndex) {
+                releaseItem(item, reusableFlag);
                 continue;
+            }
 
             visibleItems.insert(index, item);
             if (index == 0)
