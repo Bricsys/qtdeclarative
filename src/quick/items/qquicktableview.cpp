82e47c8b38117dbaafbef9234e16776d50bcf5f5
Santhosh Kumar <santhosh.kumar.selvaraj@qt.io>
2023-11-02 22:14:31 +0100

    Fix crash issue in quick table view control
    The quick table view can have both QAbstractItemModel and JS value
    as its model. The table view release and load the items (or rebuild)
    when its assigned with new model. The newmodel always be compared
    with existing model before rebuild via
    QQuickTableViewPrivate::syncModel. This comparison works with
    QAbstractItemModel but the same fails with JS model.
    
    This patch adds additional validation in
    QQuickTableViewPrivate::syncModel and QQuickTableViewPrivate::setModel
    for comparing JS model to avoid rebuild when existing model
    overwritten with same model again.
    
    Fixes: QTBUG-117917
    Pick-to: 6.2
    Change-Id: Ic15145c4b8998c68ae6471a2abf4aef727041eea
    Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>
    (cherry picked from commit 79b34126850c4235a1914e95f8e4235cfddba007)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit d02351d8debe3c089434ff6baf2caff6be52602a)

--- src/quick/items/qquicktableview.cpp
+++ src/quick/items/qquicktableview.cpp
@@ -4241,9 +4241,6 @@ QVariant QQuickTableViewPrivate::modelImpl() const
 
 void QQuickTableViewPrivate::setModelImpl(const QVariant &newModel)
 {
-    if (newModel == assignedModel)
-        return;
-
     assignedModel = newModel;
     scheduleRebuildTable(QQuickTableViewPrivate::RebuildOption::All);
     emit q_func()->modelChanged();
@@ -4251,7 +4248,7 @@ void QQuickTableViewPrivate::setModelImpl(const QVariant &newModel)
 
 void QQuickTableViewPrivate::syncModel()
 {
-    if (modelVariant == assignedModel)
+    if (compareModel(modelVariant, assignedModel))
         return;
 
     if (model) {
@@ -4525,6 +4522,13 @@ void QQuickTableViewPrivate::modelResetCallback()
     Q_Q(QQuickTableView);
     q->closeEditor();
     scheduleRebuildTable(RebuildOption::All);
+}
+
+bool QQuickTableViewPrivate::compareModel(const QVariant& model1, const QVariant& model2) const
+{
+    return (model1 == model2 ||
+            (model1.userType() == qMetaTypeId<QJSValue>() && model2.userType() == qMetaTypeId<QJSValue>() &&
+                                 model1.value<QJSValue>().strictlyEquals(model2.value<QJSValue>())));
 }
 
 void QQuickTableViewPrivate::positionViewAtRow(int row, Qt::Alignment alignment, qreal offset, const QRectF subRect)
@@ -5390,6 +5394,8 @@ QVariant QQuickTableView::model() const
 void QQuickTableView::setModel(const QVariant &newModel)
 {
     Q_D(QQuickTableView);
+    if (d->compareModel(newModel, d->assignedModel))
+        return;
 
     closeEditor();
     d->setModelImpl(newModel);
