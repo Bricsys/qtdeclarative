cb56fc7304b9d983835e8e912fa6f794c1491fd7
Santhosh Kumar <santhosh.kumar.selvaraj@qt.io>
2024-11-08 14:27:36 +0100

    Reset section drag handler to nullptr in the table view
    The section drag handler in the table view can sometimes leads to crash
    with invalid reference. Its better that we reset the state of the drag
    handler to nullptr.
    
    Change-Id: I75a10bea0f98e853a728d8376f3de78ecbc33d79
    Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>
    (cherry picked from commit b0c5efc22a10c51502892d95fe86a367b80aa678)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quick/items/qquicktableview.cpp
+++ src/quick/items/qquicktableview.cpp
@@ -7399,8 +7399,10 @@ void QQuickTableViewPrivate::initSectionDragHandler(Qt::Orientation orientation)
 
 void QQuickTableViewPrivate::destroySectionDragHandler()
 {
-    if (sectionDragHandler)
+    if (sectionDragHandler) {
         delete sectionDragHandler;
+        sectionDragHandler = nullptr;
+    }
 }
 
 void QQuickTableViewPrivate::initializeIndexMapping()
