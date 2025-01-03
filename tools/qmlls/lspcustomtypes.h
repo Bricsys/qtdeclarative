eee1fcc18b8460f5aa581cc4e41db3ed8ec78fbb
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-01-03 07:44:42 +0200

    Revert "Update commercial SPDX-License-Identifier"
    This reverts commit da5933f22c00270ac9083a089686e5c54e0057da.
    
    Revert of commercial SPDX license identifiers is required for the
    Qt 6.5.x opensource releases, Qt 6.5.4 onwards.
    
    Change-Id: Ic056fb761f242af0ec4c883ecb35d50804c1c67c
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- tools/qmlls/lspcustomtypes.h
+++ tools/qmlls/lspcustomtypes.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2022 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 #ifndef LSPCUSTOMTYPES_H
 #define LSPCUSTOMTYPES_H
 #include <QtLanguageServer/private/qlanguageserverspec_p.h>
