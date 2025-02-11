b4ee6264495ce2c2f0b2c18537d8b2465ce144ae
Doris Verria <doris.verria@qt.io>
2025-02-11 11:28:25 +0100

    tst_qquickpopup: EXPECT_FAIL several test cases for the Imagine style
    Several tests are failing with the Imagine style. Needs to be
    investigated further.
    
    Task-number: QTBUG-133530
    Change-Id: Id2a2ccc94d70de5b8c69c445066563814f6826e0
    Reviewed-by: Jan Arve Sæther <jan-arve.saether@qt.io>
    (cherry picked from commit 5c978285c4570f0ed0bc13e4d4d90f92db8c21fa)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit ab4b2457c0e48f443cf3db780f3cb5b9d9071372)

--- tests/auto/quickcontrols/qquickpopup/tst_qquickpopup.cpp
+++ tests/auto/quickcontrols/qquickpopup/tst_qquickpopup.cpp
@@ -2544,6 +2544,8 @@ void tst_QQuickPopup::popupWindowPositioning()
     // x and y properties should be 50 initially (from simplepopup.qml)
     const QPoint initialPos(50, 50);
 
+    if (QQuickStyle::name() == QLatin1String("Imagine"))
+        QEXPECT_FAIL("", "This fails with the Imagine style: QTBUG-133530", Abort);
     VERIFY_GLOBAL_POS(popup->parentItem(), popupWindow, initialPos);
     VERIFY_LOCAL_POS(popup, initialPos);
 
@@ -2625,6 +2627,8 @@ void tst_QQuickPopup::popupWindowAnchorsCenterIn()
 
     const QPoint centeredPosition(qFloor(window->width() / 2 - popupWindow->width() / 2), qFloor(window->height() / 2 - popupWindow->height() / 2));
 
+    if (QQuickStyle::name() == QLatin1String("Imagine"))
+        QEXPECT_FAIL("", "This fails with the Imagine style: QTBUG-133530", Abort);
     VERIFY_GLOBAL_POS(popup->parentItem(), popupWindow, centeredPosition);
     VERIFY_LOCAL_POS(popup, centeredPosition);
 }
@@ -2852,6 +2856,8 @@ void tst_QQuickPopup::initialPopupSize()
         auto *popupWindow = popupPrivate->popupWindow;
         QVERIFY(popupWindow);
         QVERIFY(popupWindow->isVisible());
+        if (QQuickStyle::name() == QLatin1String("Imagine"))
+            QEXPECT_FAIL("", "This fails with the Imagine style: QTBUG-133530", Abort);
         QCOMPARE(popupWindow->width(), 200);
         QCOMPARE(popupWindow->height(), 200);
     }
@@ -2894,6 +2900,8 @@ void tst_QQuickPopup::popupWindowChangingParent()
 
     const QPoint initialPos(10, 10);
 
+    if (QQuickStyle::name() == QLatin1String("Imagine"))
+        QEXPECT_FAIL("", "This fails with the Imagine style: QTBUG-133530", Abort);
     VERIFY_GLOBAL_POS(item1, popupWindow, initialPos);
     VERIFY_LOCAL_POS(popup, initialPos);
 
@@ -3064,6 +3072,8 @@ void tst_QQuickPopup::popupTypeChangeFromWindowToItem()
     const QWindow *popupWindow = popupPrivate->popupWindow;
     QVERIFY(popupWindow);
     QTRY_VERIFY(popupWindow->isVisible());
+    if (QQuickStyle::name() == QLatin1String("Imagine"))
+        QEXPECT_FAIL("", "This fails with the Imagine style: QTBUG-133530", Abort);
     QCOMPARE(popupPrivate->popupItem->position(), QPointF(0, 0));
     QVERIFY(!overlay->childItems().contains(popup->popupItem()));
 
@@ -3118,6 +3128,8 @@ void tst_QQuickPopup::popupTypeChangeFromItemToWindow()
     const QWindow *popupWindow = popupPrivate->popupWindow;
     QVERIFY(popupWindow);
     QTRY_VERIFY(popupWindow->isVisible());
+    if (QQuickStyle::name() == QLatin1String("Imagine"))
+        QEXPECT_FAIL("", "This fails with the Imagine style: QTBUG-133530", Abort);
     QCOMPARE(popupPrivate->popupItem->position(), QPointF(0, 0));
     QVERIFY(!overlay->childItems().contains(popup->popupItem()));
 
@@ -3309,6 +3321,8 @@ void tst_QQuickPopup::popupWindowPositionerRespectingScreenBounds()
     popup->setX(positionOutsideTopLeftBound.x());
     QTRY_COMPARE(xSpy.count(), 1);
     QVERIFY(QQuickTest::qWaitForPolish(window));
+    if (QQuickStyle::name() == QLatin1String("Imagine") && popupType == QQuickPopup::Window)
+        QEXPECT_FAIL("", "This fails with the Imagine style: QTBUG-133530", Abort);
     QTRY_VERIFY2(qAbs(window->contentItem()->mapToGlobal(popup->x(), popup->y()).x() - (*abf)().left()) < 2,
                  qPrintable(QStringLiteral("Expected popup's x position to be %1 but it's %2")
                             .arg((*abf)().left()).arg(window->contentItem()->mapToGlobal(popup->x(), popup->y()).x())));
