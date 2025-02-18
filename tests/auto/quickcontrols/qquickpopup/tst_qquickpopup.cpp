d994fe6208e436d5d4456cb7ceaa0b8e4f4e15b3
Santhosh Kumar <santhosh.kumar.selvaraj@qt.io>
2025-02-18 14:59:50 +0100

    Fix mouse move issue in auto test resetHoveredStateForItemsWithinPopup
    The test case tst_QQuickPopup::resetHoveredStateForItemsWithinPopup has
    been flaky sometimes, as the mouse move event has not been recognized
    by the quick control due to the reason that we generate the mouse move
    at the same point. This patch makes sure that we trigger the mouse move
    event by resetting the mouse point to a set position and further moving
    it to trigger the mouse move event.
    
    Also revert the commit 4478ebce316e063c88ac6cc9930a4b1fb39a1fd1 that
    blacklisted this test case.
    
    Task-number: QTBUG-133530
    Change-Id: Iba8ba2358088d157a90538ff45953c2b3647a0ee
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
    (cherry picked from commit 6ce8d1457a1022478c56e0a6813d1d979ee14421)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit f62efe85c88dbb96d083fff42d15425d02407222)

--- tests/auto/quickcontrols/qquickpopup/tst_qquickpopup.cpp
+++ tests/auto/quickcontrols/qquickpopup/tst_qquickpopup.cpp
@@ -3153,10 +3153,14 @@ void tst_QQuickPopup::resetHoveredStateForItemsWithinPopup()
     controlsPopup->open();
     QTRY_VERIFY(controlsPopup->isOpened());
 
-    QTest::mouseMove(window, QPoint(window->width() / 2, window->height() / 2));
+    QPoint moveStart(window->width() / 2, window->height() / 2);
+    QPoint moveEnd(moveStart.x() + 1 , moveStart.y() + 1);
+    QTest::mouseMove(window, moveStart);
+    QTest::mouseMove(window, moveEnd);
 
     auto *controlItem = qobject_cast<QQuickControl *>(controlsPopup->contentItem()->childItems().at(0));
     QVERIFY(controlItem);
+
     // Check hover enabled for the control item within the popup
     QTRY_VERIFY(controlItem->isHovered());
 
@@ -3166,6 +3170,17 @@ void tst_QQuickPopup::resetHoveredStateForItemsWithinPopup()
 
     // Control item hovered shall be disabled once we open the modal popup
     QTRY_VERIFY(!controlItem->isHovered());
+
+    blockInputPopup->close();
+    QTRY_VERIFY(!blockInputPopup->isOpened());
+    QTRY_VERIFY(!blockInputPopup->isVisible());
+
+    // Control item hovered is re-enabled after the modal popup is closed
+    QTRY_VERIFY(controlItem->isHovered());
+
+    controlsPopup->close();
+    QTRY_VERIFY(!controlsPopup->isOpened());
+    QTRY_VERIFY(!controlsPopup->isVisible());
 }
 
 void tst_QQuickPopup::noInfiniteRecursionOnParentWindowDestruction()
