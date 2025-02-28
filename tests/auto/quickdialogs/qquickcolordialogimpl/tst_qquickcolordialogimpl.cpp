f63b189874ed6de8d9d9197788042103c449a751
Frédéric Lefebvre <frederic.lefebvre@qt.io>
2025-02-28 12:27:34 +0100

    Use popupWindow()->close() to fix tst_qquickColorDialogImpl::defaults
    tst_qquickColorDialogImpl::defaults is flaky where in some cases
    dialogHelper.dialog is not being close properly.
    
    Use popupWindow()->close() instead of dialogHelper.dialog->close()
    the to properly close it.
    
    Fix flaky test tst_qquickColorDialogImpl::defaults on Opensuse
    
    Pick-to: 6.5
    Change-Id: Ic62e241e12f1289d2aaa11552047b61d99c056d9
    Reviewed-by: Oliver Eftevaag <oliver.eftevaag@qt.io>
    (cherry picked from commit f3a8eb7270f32ae8509744e68ff7703019100430)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 83fee39ccce07eaea140fb2df20947cca5d9b772)

--- tests/auto/quickdialogs/qquickcolordialogimpl/tst_qquickcolordialogimpl.cpp
+++ tests/auto/quickdialogs/qquickcolordialogimpl/tst_qquickcolordialogimpl.cpp
@@ -127,7 +127,7 @@ void tst_QQuickColorDialogImpl::defaults()
     QVERIFY2(!alphaSlider->isVisible(),
              "The AlphaSlider should not be visible unless the ShowAlphaChannel options has "
              "explicitly been set");
-    dialogHelper.dialog->close();
+    dialogHelper.popupWindow()->close();
     QTRY_VERIFY(!dialogHelper.isQuickDialogOpen());
     dialogHelper.dialog->setOptions(QColorDialogOptions::ShowAlphaChannel);
     QVERIFY(dialogHelper.openDialog());
@@ -143,7 +143,7 @@ void tst_QQuickColorDialogImpl::defaults()
         QVERIFY2(eyeDropperButton->isVisible(),
                  "The Eye Dropper Button should be visible unless the NoEyeDropperButton option has "
                  "explicitly been set");
-        dialogHelper.dialog->close();
+        dialogHelper.popupWindow()->close();
         QTRY_VERIFY(!dialogHelper.isQuickDialogOpen());
         dialogHelper.dialog->setOptions(QColorDialogOptions::NoEyeDropperButton);
         QVERIFY(dialogHelper.openDialog());
