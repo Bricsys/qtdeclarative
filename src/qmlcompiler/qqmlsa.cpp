83eda05c84a7ee878afbbb610028ae0d1a04a57f
Topi Reinio <topi.reinio@qt.io>
2025-03-06 13:15:17 +0000

    Doc: Fix issues with \fn signatures
    Move QQmlSA::LoggerWarningId function documentation out of the
    QQmlJS namespace, as that causes match failures with QDoc.
    
    Qualify QQmlSA::Method and Element \fn signatures with the correct
    scope to help QDoc match them.
    
    Task-number: QTBUG-134250
    Change-Id: I89407afe251158d99a93f44c25bb6db1a83a6024
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>
    Reviewed-by: Olivier De Cannière <olivier.decanniere@qt.io>
    (cherry picked from commit f1862814b0c6f7bdd304119270fc7554411ff598)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 6cf992796781ad3060196e6229b547019a97ab4e)

--- src/qmlcompiler/qqmlsa.cpp
+++ src/qmlcompiler/qqmlsa.cpp
@@ -452,7 +452,7 @@ QMultiHash<QString, Method>::const_iterator Method::Methods::constBegin() const
 }
 
 /*!
-    \fn QMultiHash<QString, Method>::const_iterator Methods::begin() const
+    \fn QMultiHash<QString, QQmlSA::Method>::const_iterator QQmlSA::Method::Methods::begin() const
     Returns an iterator to the beginning of the methods.
  */
 
@@ -471,7 +471,7 @@ QMultiHash<QString, Method>::const_iterator Method::Methods::constEnd() const
 }
 
 /*!
-    \fn QMultiHash<QString, Method>::const_iterator Methods::end() const
+    \fn QMultiHash<QString, QQmlSA::Method>::const_iterator QQmlSA::Method::Methods::end() const
     Returns an iterator to the end of the methods.
  */
 
@@ -839,7 +839,7 @@ Element &Element::operator=(const Element &other)
 }
 
 /*!
-    \fn Element &Element::operator=(Element &&) noexpect;
+    \fn QQmlSA::Element &QQmlSA::Element::operator=(QQmlSA::Element &&other)
     Move-assigns \a other to this Element instance.
  */
 
