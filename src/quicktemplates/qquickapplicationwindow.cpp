2a741a3f0cddcc4b9e1ffa5195c7425a7d4d016d
Juha Vuolle <juha.vuolle@qt.io>
2025-04-14 09:16:04 +0300

    Fix compilation when quicktemplates2_hover feature is disabled
    Pick-to: 6.9 6.8
    Fixes: QTBUG-135946
    Change-Id: Ie22da6a87d3cb43601ae9df30cb99262a8f6b479
    Reviewed-by: Oliver Eftevaag <oliver.eftevaag@qt.io>

--- src/quicktemplates/qquickapplicationwindow.cpp
+++ src/quicktemplates/qquickapplicationwindow.cpp
@@ -822,6 +822,7 @@ void QQuickApplicationWindow::classBegin()
     // them.
     d->control = new QQuickControl(QQuickWindow::contentItem());
     d->control->setObjectName("ApplicationWindowContentControl");
+#if QT_CONFIG(quicktemplates2_hover)
     // Now that QQuickContentItem's parent is the content control rather
     // than QQuickRootItem, we need to ensure that the control has the
     // correct hoverEnabled value. If we don't do this (get the value
@@ -829,6 +830,7 @@ void QQuickApplicationWindow::classBegin()
     // and some controls won't be hoverable when they should be.
     QQuickControlPrivate::get(d->control)->updateHoverEnabled(
         QQuickControlPrivate::calcHoverEnabled(nullptr), false);
+#endif
     auto *contentItem = new QQuickContentItem(this, d->control);
     // The content item can't be its own focus scope here, as that
     // will detach focus of items inside the content item from focus
