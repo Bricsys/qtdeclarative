Bricsys/qtdeclarative

commit 137cdc364dd5d569c613b83246a26e3803d7556d

CMake: add a DISCARD_QML_CONTENTS option to qt_add_qml_module
The original content of QML files seem of no use when qmlcachegen is
enabled.

DISCARD_QML_CONTENTS relies on the new qt_add_resources
DISCARD_FILE_CONTENTS option.

[ChangeLog][CMake] Added a DISCARD_QML_CONTENTS option to
qt_add_qml_module(), that removes original QML and JS file contents from
the target's resource system.

Change-Id: I490c0fcbb198fcb66cc033c71ea1287891e21369
Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

src/qml/Qt6QmlMacros.cmake
@@ -232,6 +232,7 @@ function(qt6_add_qml_module target)
         NO_CACHEGEN
         NO_RESOURCE_TARGET_PATH
         NO_IMPORT_SCAN
+        DISCARD_QML_CONTENTS
         ENABLE_TYPE_COMPILER
 
         # Used to mark modules as having static side effects (i.e. if they install an image provider)
@@ -748,6 +749,7 @@ Check https://doc.qt.io/qt-6/qt-cmake-policy-qtp0001.html for policy details."
         QT_QML_MODULE_NO_PLUGIN "${arg_NO_PLUGIN}"
         QT_QML_MODULE_NO_PLUGIN_OPTIONAL "${arg_NO_PLUGIN_OPTIONAL}"
         QT_QML_MODULE_NO_IMPORT_SCAN "${arg_NO_IMPORT_SCAN}"
+        QT_QML_MODULE_DISCARD_QML_CONTENTS "${arg_DISCARD_QML_CONTENTS}"
         _qt_qml_module_follow_foreign_versioning "${arg_FOLLOW_FOREIGN_VERSIONING}"
         QT_QML_MODULE_URI "${arg_URI}"
         QT_QML_MODULE_TARGET_PATH "${arg_TARGET_PATH}"
@@ -2923,6 +2925,7 @@ function(qt6_target_qml_sources target)
     get_target_property(no_cachegen            ${target} QT_QML_MODULE_NO_CACHEGEN)
     get_target_property(no_qmldir              ${target} QT_QML_MODULE_NO_GENERATE_QMLDIR)
     get_target_property(no_extra_qmldirs       ${target} QT_QML_MODULE_NO_GENERATE_EXTRA_QMLDIRS)
+    get_target_property(discard_qml_contents   ${target} QT_QML_MODULE_DISCARD_QML_CONTENTS)
     get_target_property(resource_prefix        ${target} QT_QML_MODULE_RESOURCE_PREFIX)
     get_target_property(qml_module_version     ${target} QT_QML_MODULE_VERSION)
     get_target_property(past_major_versions    ${target} QT_QML_MODULE_PAST_MAJOR_VERSIONS)
@@ -3496,21 +3499,43 @@ function(qt6_target_qml_sources target)
     if(NOT counter)
         set(counter 0)
     endif()
-    set(resource_name ${target}_raw_qml_${counter})
-    set(resource_targets)
-    qt6_add_resources(${target} ${resource_name}
-        PREFIX ${arg_PREFIX}
-        FILES ${arg_QML_FILES} ${arg_RESOURCES}
-        OUTPUT_TARGETS resource_targets
-    )
-    list(APPEND output_targets ${resource_targets})
 
+    set(qml_resource_name ${target}_raw_qml_${counter})
+    set(qml_resource_targets)
+    set(qml_resource_options)
+    if(discard_qml_contents)
+        list(APPEND qml_resource_options DISCARD_FILE_CONTENTS)
+    endif()
+    qt6_add_resources(${target} ${qml_resource_name}
+        PREFIX ${arg_PREFIX}
+        FILES ${arg_QML_FILES}
+        OUTPUT_TARGETS qml_resource_targets
+        ${qml_resource_options}
+    )
+    list(APPEND output_targets ${qml_resource_targets})
     # Save the resource name in a property so we can reference it later in a qml plugin
     # constructor, to avoid discarding the resource if it's in a static library.
     __qt_internal_sanitize_resource_name(
-        sanitized_resource_name "${resource_name}")
-    set_property(TARGET ${target}
-        APPEND PROPERTY _qt_qml_module_sanitized_resource_names "${sanitized_resource_name}")
+        sanitized_qml_resource_name "${qml_resource_name}")
+    set_property(TARGET ${target} APPEND PROPERTY
+        _qt_qml_module_sanitized_resource_names "${sanitized_qml_resource_name}")
+
+    if(arg_RESOURCES)
+        set(resources_resource_name ${target}_raw_res_${counter})
+        set(resources_resource_targets)
+        qt6_add_resources(${target} ${resources_resource_name}
+            PREFIX ${arg_PREFIX}
+            FILES ${arg_RESOURCES}
+            OUTPUT_TARGETS resources_resource_targets
+        )
+        list(APPEND output_targets ${resources_resource_targets})
+        # Save the resource name in a property so we can reference it later in a qml plugin
+        # constructor, to avoid discarding the resource if it's in a static library.
+        __qt_internal_sanitize_resource_name(
+            sanitized_resources_resource_name "${resources_resource_name}")
+        set_property(TARGET ${target} APPEND PROPERTY
+            _qt_qml_module_sanitized_resource_names "${sanitized_resources_resource_name}")
+    endif()
 
     if(extra_qmldirs AND NOT no_extra_qmldirs)
         list(REMOVE_DUPLICATES extra_qmldirs)
@@ -3534,7 +3559,7 @@ Check https://doc.qt.io/qt-6/qt-cmake-policy-qtp0004.html for policy details."
             endforeach()
 
             set(extra_qmldirs_targets)
-            qt6_add_resources(${target} "${resource_name}_extra_qmldirs"
+            qt6_add_resources(${target} "${qml_resource_name}_extra_qmldirs"
                 PREFIX ${arg_PREFIX}
                 FILES ${extra_qmldirs}
                 BASE ${output_dir}
@@ -3547,10 +3572,9 @@ Check https://doc.qt.io/qt-6/qt-cmake-policy-qtp0004.html for policy details."
             # Save the resource name in a property so we can reference it later in a qml plugin
             # constructor, to avoid discarding the resource if it's in a static library.
             __qt_internal_sanitize_resource_name(
-                sanitized_extra_qmldirs_resource_name "${resource_name}_extra_qmldirs")
-            set_property(TARGET ${target}
-                APPEND PROPERTY _qt_qml_module_sanitized_resource_names
-                "${sanitized_extra_qmldirs_resource_name}")
+                sanitized_extra_qmldirs_resource_name "${qml_resource_name}_extra_qmldirs")
+            set_property(TARGET ${target} APPEND PROPERTY
+                _qt_qml_module_sanitized_resource_names "${sanitized_extra_qmldirs_resource_name}")
         endif()
     endif()
 

src/qml/doc/src/cmake/qt_add_qml_module.qdoc
@@ -49,6 +49,7 @@ qt_add_qml_module(
     [NO_CACHEGEN]
     [NO_RESOURCE_TARGET_PATH]
     [NO_IMPORT_SCAN]
+    [DISCARD_QML_CONTENTS]
     [ENABLE_TYPE_COMPILER]
     [TYPE_COMPILER_NAMESPACE namespace]
     [QMLTC_EXPORT_DIRECTIVE export_macro]
@@ -739,6 +740,17 @@ disabled by providing the \c{NO_IMPORT_SCAN} option. Doing so means the project
 takes on the responsibility of ensuring all required plugins are instantiated
 and linked for static builds. For non-static builds the project must manually
 work out and deploy all QML modules used by an executable target.
+
+\section2 DISCARD_QML_CONTENTS
+
+\target DISCARD_QML_CONTENTS
+By default, QML and JS source file contents are included in the target's resource system.
+Use \c DISCARD_QML_CONTENTS to remove these contents and reduce the binary size.
+
+\note If you omit the source code from the binary, the QML engine has to
+rely on the compilation units created by \l{qmlcachegen} or \l{qmlsc}.
+Those are tied to the specific version of Qt they were built with. If you change
+the version of Qt your application uses, they can't be loaded anymore.
 
 \section2 Arguments for qmltc
 

commit e7e9d00930895a4d5e10ba743ad550929e06451b

CMake: Fix qt6_target_qml_sources without QML_FILES argument
Commit 137cdc364dd5d569c613b83246a26e3803d7556d broke
qt6_target_qml_sources with no QML_FILES argument, because it called
qt6_add_resources without FILES (for the QML files), which causes a
linker error.

Check whether we have QML files and only then create the QML-specific
resource.

Add an autotest that calls qt6_target_qml_sources separately for QML and
resource files.

Fixes: QTBUG-136491
Change-Id: I5e8173652887f257fd6d8e6c8d18bcc0e295b714
Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>

src/qml/Qt6QmlMacros.cmake
@@ -3637,24 +3637,26 @@ function(qt6_target_qml_sources target)
     endif()
 
     set(qml_resource_name ${target}_raw_qml_${counter})
-    set(qml_resource_targets)
-    set(qml_resource_options)
-    if(discard_qml_contents)
-        list(APPEND qml_resource_options DISCARD_FILE_CONTENTS)
+    if(actual_QML_FILES)
+        set(qml_resource_targets "")
+        set(qml_resource_options "")
+        if(discard_qml_contents)
+            list(APPEND qml_resource_options DISCARD_FILE_CONTENTS)
+        endif()
+        qt6_add_resources(${target} ${qml_resource_name}
+            PREFIX ${arg_PREFIX}
+            FILES ${actual_QML_FILES}
+            OUTPUT_TARGETS qml_resource_targets
+            ${qml_resource_options}
+        )
+        list(APPEND output_targets ${qml_resource_targets})
+        # Save the resource name in a property so we can reference it later in a qml plugin
+        # constructor, to avoid discarding the resource if it's in a static library.
+        __qt_internal_sanitize_resource_name(
+            sanitized_qml_resource_name "${qml_resource_name}")
+        set_property(TARGET ${target} APPEND PROPERTY
+            _qt_qml_module_sanitized_resource_names "${sanitized_qml_resource_name}")
     endif()
-    qt6_add_resources(${target} ${qml_resource_name}
-        PREFIX ${arg_PREFIX}
-        FILES ${actual_QML_FILES}
-        OUTPUT_TARGETS qml_resource_targets
-        ${qml_resource_options}
-    )
-    list(APPEND output_targets ${qml_resource_targets})
-    # Save the resource name in a property so we can reference it later in a qml plugin
-    # constructor, to avoid discarding the resource if it's in a static library.
-    __qt_internal_sanitize_resource_name(
-        sanitized_qml_resource_name "${qml_resource_name}")
-    set_property(TARGET ${target} APPEND PROPERTY
-        _qt_qml_module_sanitized_resource_names "${sanitized_qml_resource_name}")
 
     if(actual_RESOURCES)
         set(resources_resource_name ${target}_raw_res_${counter})

tests/auto/cmake/CMakeLists.txt
@@ -147,6 +147,8 @@ if(TARGET Qt::Qml)
         BUILD_OPTIONS -Drun=case_resources
         ${test_duplicate_files_extra_args}
     )
+
+    _qt_internal_test_expect_pass(test_separate_qml_and_resources)
 endif()
 
 if(TARGET Qt::Quick)

tests/auto/cmake/test_separate_qml_and_resources/CMakeLists.txt
@@ -1,0 +1,27 @@
+cmake_minimum_required(VERSION 3.16)
+project(test_separate_qml_and_resources LANGUAGES CXX)
+
+find_package(Qt6 REQUIRED COMPONENTS Core Gui Qml Test)
+qt_standard_project_setup(REQUIRES 6.10)
+
+qt_add_qml_module(MyQmlModule
+    STATIC
+    URI org.qt-project.test_separate_qml_and_resources
+    VERSION 1.0
+)
+
+qt_target_qml_sources(MyQmlModule
+    QML_FILES
+        Main.qml
+)
+
+qt_target_qml_sources(MyQmlModule
+    RESOURCES
+        file.txt
+)
+
+add_executable(app
+    main.cpp
+)
+
+target_link_libraries(app PRIVATE MyQmlModule MyQmlModuleplugin)

tests/auto/cmake/test_separate_qml_and_resources/Main.qml
@@ -1,0 +1,7 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+import QtQuick
+
+Item {
+}

tests/auto/cmake/test_separate_qml_and_resources/main.cpp
@@ -1,0 +1,4 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+int main() {}