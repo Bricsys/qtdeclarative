5ee2001c6c5105b0456df354b8e526d000db7e05
Fabian Kosmale <fabian.kosmale@qt.io>
2025-07-28 15:24:53 +0200

    qmllint: Do not warn about unknown signal handlers in custom parsers
    A custom parser can do arbitrary things with signal handlers inside it,
    even if no corresponding signal exists in itself. For instance,
    PropertyChanges allows to set signals on its target.
    
    Task-number: QTBUG-138173
    Change-Id: I8b92cb92dad8d9a53cb57b4a9ad3d2d48b24c08a
    Reviewed-by: Sami Shalayel <sami.shalayel@qt.io>
    Reviewed-by: Olivier De Cannière <olivier.decanniere@qt.io>
    (cherry picked from commit 9ae8312c0d9a74ca12ed3617519b73c182c201f8)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/qml/qmllint/tst_qmllint.cpp
+++ tests/auto/qml/qmllint/tst_qmllint.cpp
@@ -1972,6 +1972,8 @@ void TestQmllint::cleanQmlCode_data()
     QTest::newRow("BindingTypeMismatchFunction") << QStringLiteral("bindingTypeMismatchFunction.qml");
     QTest::newRow("BindingsOnGroupAndAttachedProperties")
             << QStringLiteral("goodBindingsOnGroupAndAttached.qml");
+    QTest::newRow("CustomParserSignalHandler")
+            << QStringLiteral("customParserSignalHandler.qml");
     QTest::newRow("CustomParserUnqualifiedAccess")
             << QStringLiteral("customParserUnqualifiedAccess.qml");
     QTest::newRow("duplicateImportsClean") << QStringLiteral("duplicateImportsClean.qml");
