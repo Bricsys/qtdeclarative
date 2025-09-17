3cba925cddf5ae4f2d566554895f800c8fe88384
Dilek Akcay <dilek.akcay@qt.io>
2025-09-17 09:45:19 +0200

    SearchField: Keep currentIndex at -1 until user explicitly selects
    Until now, currentIndex was assigned to different values on different
    actions, like to 0 on model changes, or to the index of the highlighted
    index according to the arrow key actions. These caused unexpected behavior
    so the following changes were made:
    
    - Removed setting to 0 when model is set in the beginning.
    - Reset currentIndex to -1 on any user edit(typing/deleting)
    - Disabled arrow key interactions on currentIndex
    
    Now currentIndex only changes whether the user clicks an item in the
    popup or presses Enter on a highlighted item.
    
    Change-Id: Ib1a991c87a767fe436b4b025a26834871401ca4b
    Reviewed-by: Jan Arve Sæther <jan-arve.saether@qt.io>
    (cherry picked from commit 530189838a00ee15c6c824a7d07ea7a54b98ed32)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quicktemplates/qquicksearchfield.cpp
+++ src/quicktemplates/qquicksearchfield.cpp
@@ -376,9 +376,6 @@ void QQuickSearchFieldPrivate::increaseCurrentIndex()
     if (isPopupVisible()) {
         if (highlightedIndex < q->suggestionCount() - 1)
             setHighlightedIndex(highlightedIndex + 1, Highlight);
-    } else {
-        if (currentIndex < q->suggestionCount() - 1)
-            setCurrentItemAtIndex(currentIndex + 1, Activate);
     }
 }
 
@@ -387,9 +384,6 @@ void QQuickSearchFieldPrivate::decreaseCurrentIndex()
     if (isPopupVisible()) {
         if (highlightedIndex > 0)
             setHighlightedIndex(highlightedIndex - 1, Highlight);
-    } else {
-        if (currentIndex > 0)
-            setCurrentItemAtIndex(currentIndex - 1, Activate);
     }
 }
 
@@ -420,7 +414,17 @@ void QQuickSearchFieldPrivate::setCurrentItemAtIndex(int index, Activation activ
 
 void QQuickSearchFieldPrivate::updateHighlightedIndex()
 {
-    setHighlightedIndex(popup->isVisible() ? currentIndex : -1, NoHighlight);
+    Q_Q(QQuickSearchField);
+    int index = -1;
+
+    if (isPopupVisible()) {
+        if (currentIndex >= 0)
+            index = currentIndex;
+        else if (q->suggestionCount() > 0)
+            index = 0; // auto-highlight first suggestion
+    }
+
+    setHighlightedIndex(index, NoHighlight);
 }
 
 void QQuickSearchFieldPrivate::setHighlightedIndex(int index, Highlighting highlight)
@@ -501,6 +505,9 @@ void QQuickSearchFieldPrivate::updateText()
     if (text != textInput) {
         q->setText(textInput);
         emit q->textEdited();
+
+        setCurrentIndex(-1);
+        updateHighlightedIndex();
 
         if (live)
             emit q->searchTriggered();
@@ -720,9 +727,6 @@ void QQuickSearchField::setSuggestionModel(const QVariant &model)
     d->suggestionModel = suggestionModel;
     d->createDelegateModel();
     emit suggestionCountChanged();
-    if (isComponentComplete()) {
-        setCurrentIndex(suggestionCount() > 0 ? 0 : -1);
-    }
     emit suggestionModelChanged();
 }
 
@@ -1114,15 +1118,11 @@ void QQuickSearchField::keyPressEvent(QKeyEvent *event)
         case Qt::Key_Home:
             if (d->isPopupVisible())
                 d->setHighlightedIndex(0, Highlight);
-            else
-                d->setCurrentItemAtIndex(0, Activate);
             event->accept();
             break;
         case Qt::Key_End:
             if (d->isPopupVisible())
                 d->setHighlightedIndex(suggestionCount() - 1, Highlight);
-            else
-                d->setCurrentItemAtIndex(suggestionCount() - 1, Activate);
             event->accept();
             break;
         default:
@@ -1156,11 +1156,6 @@ void QQuickSearchField::componentComplete()
 
     if (d->delegateModel && d->ownModel)
         static_cast<QQmlDelegateModel *>(d->delegateModel)->componentComplete();
-
-    if (suggestionCount() > 0) {
-        if (!d->hasCurrentIndex && d->currentIndex == -1)
-            setCurrentIndex(0);
-    }
 }
 
 void QQuickSearchField::contentItemChange(QQuickItem *newItem, QQuickItem *oldItem)
