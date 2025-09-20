d43f407d85fd216e889cec6a6c4b10d2f4821542
Tim Blechmann <tim.blechmann@qt.io>
2025-09-20 09:07:11 +0800

    Native Style: simplify implementation of QQuickNativeStyle
    * use unique_ptr for memory management
    * declare static member as inline to avoid slim down the builds a bit
    
    Change-Id: I3406fb2e5f027303451b0e08de6a565195f23a55
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 71f7260b19dc4e31f1563c877d03077be31234e0)
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/quicknativestyle/qstyle/qquicknativestyle.h
+++ src/quicknativestyle/qstyle/qquicknativestyle.h
@@ -6,6 +6,8 @@
 
 #include "qquickstyle.h"
 
+#include <memory>
+
 QT_BEGIN_NAMESPACE
 
 namespace QQC2 {
@@ -13,20 +15,11 @@ namespace QQC2 {
 class QQuickNativeStyle
 {
 public:
-    static void setStyle(QStyle *style)
-    {
-        if (s_style)
-            delete s_style;
-        s_style = style;
-    }
-
-    inline static QStyle *style()
-    {
-        return s_style;
-    }
+    static void setStyle(QStyle *style) { s_style.reset(style); }
+    static QStyle *style() { return s_style.get(); }
 
 private:
-    static QStyle *s_style;
+    static inline std::unique_ptr<QStyle> s_style;
 };
 
 } // namespace QQC2
